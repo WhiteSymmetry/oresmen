e17136201546595c7abfd7ee03ee7d667b4a5895
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-07 14:26:10 +0300

    Bump version to 0.1.6 and update dependencies

--- src/oresmen/_version.py
+++ src/oresmen/_version.py
@@ -1,9 +1,9 @@
 # _version.py
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __license__ = "AGPL3.0 or later"
 __description__ = "A module for generating Oresme numbers (harmonic series partial sums)"
 __author__ = "Mehmet Keçeci"
 __url__ = "https://github.com/WhiteSymmetry/oresmen"
 __docs__ = "https://WhiteSymmetry.github.io/oresmen"  # Opsiyonel: Dokümantasyon linki
-__dependencies__ = ["python>=3.10"]  # Diğer bağımlılıkları da ekleyebilirsiniz
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz
