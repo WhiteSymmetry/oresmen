0dc4887bf8f8bfbed448cd021e1a23f2566d8a13
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-06-08 22:04:59 +0300

    Add version information to _version.py

---
+++ oresmen/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "0.1.7"
+__license__ = "AGPL3.0 or later"
+__description__ = "A module for generating Oresme numbers (harmonic series partial sums)"
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/oresmen"
+__docs__ = "https://WhiteSymmetry.github.io/oresmen"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz
